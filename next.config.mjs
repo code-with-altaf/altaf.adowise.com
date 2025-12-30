49e8d7f16c2e9f48a52437434c9f7d9f1160eec1
code-with-altaf <reachmohdaltaf@gmail.com>
2025-12-31 04:08:20 +0530

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  images: {
+    domains: [
+      "adowise.com",
+      "www.tradylytics.in",
+      "www.explorin.io",
+      "blog-app-eta-ten.vercel.app",
+      "briefly-kappa.vercel.app",
+      "connectify-h8zy.onrender.com",
+      "yt3.googleusercontent.com"
+    ]
+  }
 };
 
 export default nextConfig;
